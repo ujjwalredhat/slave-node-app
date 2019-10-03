a71a75fb625104539f3618dbc72276db535b5751
Mark Roberts <marrober@redhat.com>
2019-10-03 16:47:32 +0100

    Initial version of slave

---
+++ slave.js
@@ -0,0 +1,37 @@
+const express = require('express');
+var ip = require("ip");
+const app = express();
+const router = express.Router();
+var bunyan = require('bunyan');
+const uuid = require('uuid')
+const id = uuid.v1()
+
+//var PropertiesReader = require('properties-reader');
+//var properties = PropertiesReader('../application.properties');
+
+var log = bunyan.createLogger({
+      name: "Slave",
+      streams: [{
+      path: './slave.log',
+    }]
+  });
+
+var targePort = process.env.NODE_APP_SLAVE_SERVICE_PORT;
+//var targePort = properties.get('slave.node.targetPort');
+
+const port = targePort;
+var counter = 0;
+
+app.get('/', (request, response) => response.send('.. slave is running'));
+
+app.get('/ip', (request, response) => {
+  var messageText = ip.address();
+  counter++;
+  log.info({app: 'slave', phase: 'operational', id: id, counter: counter, slave_ip: ip.address()}, " responded .... " + counter);
+  response.json(messageText);
+});
+
+log.info( ip.address() );
+
+// set the server to listen on the designated port
+app.listen(port, () => log.info({app: 'slave', phase: 'setup', }, `Listening on port ${port}`));
